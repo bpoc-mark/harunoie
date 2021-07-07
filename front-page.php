e6734be43611ad0b104fbaea69e8c9e58140234b
arsua-greg <gregarsua@bpoc.co.jp>
2021-07-07 16:06:06 +0800

    finish pc version

--- front-page.php
+++ front-page.php
@@ -1,12 +1,210 @@
-<?php get_header();?>
-    <section>1</section>
-    <section>2</section>
-    <section>3</section>
-    <section>4</section>
-    <section>5</section>
-    <section>6</section>
-    <section>7</section>
-    <section>8</section>
-    <section>9</section>
-    <section>10</section>
-<?php get_footer();?>
+<?php get_header(); ?>
+<section>1</section>
+<section>2</section>
+<section>3</section>
+<section>4</section>
+<section>5</section>
+<section class="sect_6">
+    <img class="sect_6__image" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/asset_1.png" alt="">
+    <div class="l-wrap">
+        <div class="heat">
+            <div class="heat__col-1">
+                <p class="title">熱性能「HEAT20 G2」</p>
+                <p class="content">はるのいえの企画住宅は、『HEAT20 G2』が標準仕様。
+                    『HEAT20』とは住宅に関する有識者を集め2009年にできた民間団体で、断熱性能に関して「G1」「G2」「G3」の3段階の基準が設定されています。
+                    各地域における代表的な暖房方式を調査・検討したうえで、「冬期間の室内温度環境」と「省エネルギー性能」を踏まえて設定されており、 G1＜G2＜G3と数字が大きくなるほど断熱性能は高くなります。
+                    <br><br>
+                    現在、日本には大きく分けて3つの「断熱性能の基準」があります。
+                    なかでも、もっとも低い基準が『建築物のエネルギー消費性能の向上に関する法律（建築物省エネ法）』による「平成28年基準」。
+                    その次が「ZEH（ゼッチ）基準」。 ZEH（ゼッチ）とは“ネット・ゼロ・エネルギー・ハウス”の略で、消費エネルギーよりもつくるエネルギーが多くなることを目指した住宅のこと。
+                    そして、最も高性能な基準が「HEAT20の基準」です。
+                </p>
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/sect_6_table.png" alt="">
+                <p class="g2">『HEAT20 G2』対応の、弊社の断熱仕様は以下のとおり。</p>
+
+                <table>
+                    <tr>
+                        <td>基礎</td>
+                        <td>スタイロフォーム100mm</td>
+                    </tr>
+                    <tr>
+                        <td>天井</td>
+                        <td>高性能グラスウール16K、310mm</td>
+                    </tr>
+                    <tr>
+                        <td>壁</td>
+                        <td>ネオマフォーム66mm＋45mmのダブル断熱</td>
+                    </tr>
+                    <tr>
+                        <td>窓</td>
+                        <td>樹脂製サッシ/トリプルガラス、複層ガラス</td>
+                    </tr>
+                </table>
+                <p class="note">注）今後、変更の可能性もあります。</p>
+                <p class="heat20">なお、はるのいえでは建物の外皮性能だけでなく
+                    <span class="heat_loss">日射熱・生活熱・換気による熱損失</span>
+                    など、さまざまな要素を加味してシミュレーションを繰り返すことで、『HEAT20』の厳しい水準をクリアしています。
+                </p>
+            </div>
+            <div class="heat__divider">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/divider.png" alt="">
+            </div>
+            <div class="heat__col-2">
+                <p class="title">耐震等級「3」</p>
+                <p class="content">建築基準法で規定された耐震性能は「震度6～7の大地震でも倒壊・崩壊しない」ことを基準としたもの。
+                    これは『耐震等級1』に該当し、3段階に分かれた耐震等級の中でもっとも性能が低いと位置付けられています。
+                </p>
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/sect_6_table-2.png" alt="">
+                <p class="seismic_grade">
+                    震度7の大地震で倒壊しないなら、『耐震等級1』で十分と考える方もいらっしゃるかもしれません。
+                    しかし、2016年の熊本地震では繰り返す揺れによって『耐震等級2』の住宅が倒壊しています。
+                    これにより、『耐震等級1』はあくまでも避難時間をかせぐための耐震性能に過ぎず、その後も住み続ける上では、強度として十分でないことが実証されました。
+                    <br><br>
+                    はるのいえの企画住宅は、『
+                    <span>耐震等級3</span>
+                    』が
+                    <span>標準仕様。</span>
+                    （※条件によっては対応できない場合がございます）
+                    壁量計算だけでなく、しっかりと
+                    <span>構造計算</span>
+                    （許容応力度計算）をして地震に対する耐力を確認しています。
+                    <br><br>
+                    なお、『耐震等級3相当』とは、耐震等級3と同等の性能を持ってはいるものの、住宅性能評価機関への申請をせず、正式な認定を受けていない建物のことをいいます。
+                    <br><br>
+                    『耐震等級3』の認定を受けた場合、地震保険の掛け金が50％に割引されるほか、フラット35の融資を受ける際に金利優遇がある、贈与税の非課税枠が加算対象となるなどのメリットも。
+                    <br>
+                    <br>
+                    より高い安心を求める方は、構造計算によって強度を証明された、はるのいえの高耐震住宅をご検討ください。
+
+                </p>
+            </div>
+        </div>
+
+        <div class="fuel">
+            <div class="fuel__col-1">
+                <p class="title">燃費で家の性能を比べる</p>
+                <p class="desc">「PLAN01」に暮らす家庭での１年間の光熱費を想定した結果です。<br>
+                    省エネ基準の家との差を比べてみてください。
+                </p>
+            </div>
+
+            <div class="fuel__col-2">
+                <p class="title">※光熱費の計算は下表の単価設定により算定することを基本とします。</p>
+                <img class="table-3" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/sect_6_table-3.png" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_6/sect_6_table-4.png" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
+<section class="sect_7">
+    <div class="l-wrap">
+
+        <div class="steps">
+            <!-- STEP 1 -->
+            <div class="procedure__left afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">ヒアリング</p>
+                    </div>
+                    <p class="desc">まずは、あなたの理想の家づくりについてお聞かせください。
+                        こだわりや予算をお伺いし、企画住宅で話を進めたいということであれば、この時点である程度プランの目星をつけておきます。<br>
+                        お打ち合わせは対面でもオンラインでもOK。ご家族皆さまでご参加ください。
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 2 -->
+            <div class="procedure__right afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">資金計画</p>
+                    </div>
+                    <p class="desc">家づくりの基本は資金計画。
+                        はるのいえの資金計画は「家を建てるための計画」ではなく、「家を建てても今の生活をキープしていくための計画」です。
+                        20年後、30年後まで考えた、無理のない資金計画をご提案いたします。
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 3 -->
+            <div class="procedure__left afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">土地探し＋プラン選び（VR）</p>
+                    </div>
+                    <p class="desc">これから土地を購入されるお客さまには、土地探しもサポートいたします。<br>
+                        土地の形状によって配置できるプランも変わってきますので、土地探しと並行してプラン選びを進めていきましょう。
+                        プランごとのVR（バーチャル・リアリティ）もご体験いただけます！
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 4 -->
+            <div class="procedure__right afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">シミュレーション</p>
+                    </div>
+                    <p class="desc">間取りや仕様が同じ建物であっても、敷地が違えば日照や通風の条件は異なります。<br>
+                        最適な温熱環境を整えるため、企画住宅であっても日当たりや風通し、建物燃費などのシミュレーションはしっかりと行います。
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 5 -->
+            <div class="procedure__left afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">着工</p>
+                    </div>
+                    <p class="desc">プラン決定後、構造計算や各種申請業務などに1ヶ月半〜程度のお時間をいただきます。<br>
+                        施工期間中は工事の進捗状況をこまめにお知らせいたしますので、安心しておまかせください。
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 6 -->
+            <div class="procedure__right afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">自社検査＋第三者機関による検査</p>
+                    </div>
+                    <p class="desc">はるのいえでは、自社検査のほか第三者機関による検査も行っています。<br>
+                        検査は全7回177項目。雨漏りや水漏れしやすい部分と躯体の強度、省エネに関する部分を重点的にチェックします。
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 7 -->
+            <div class="procedure__left afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">お引き渡し</p>
+                    </div>
+                    <p class="desc">完成検査が終わったら、いよいよお引き渡しです。<br>
+                        夏涼しく冬暖かい、省エネの家。はるのいえの企画住宅で、心地よいくらしをスタートしてください！
+                    </p>
+                </div>
+            </div>
+
+            <!-- STEP 8 -->
+            <div class="procedure__right afterimage">
+                <div class="procedure__item">
+                    <div class="row-1">
+                        <p class="title">アフターサービス</p>
+                    </div>
+                    <p class="desc">半年、1年、2年、5年、10年の全5回、第三者機関がお住まいの点検に訪問いたします。<br>
+                        保証は維持管理型保証（最大50年保証）、短期保証（2年）のほか、保険代理店との提携による充実した安心サービスもご案内しております。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sect_8">
+    <iframe width="100%" height="500" src="https://www.youtube.com/embed/ILgsuZ0jWdI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</section>
+<section>9</section>
+<section>10</section>
+<?php get_footer(); ?>
