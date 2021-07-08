55ce8f304d2e2749ca407a8a03303e1522b5e037
bpoc-mark <markariel.maata@gmail.com>
2021-07-08 10:00:54 +0800

    added section 2 pc and so view / adjust header logo on sp view

--- front-page.php
+++ front-page.php
@@ -30,7 +30,82 @@
             </div>
         </div>
     </section>
-    <section>2</section>
+    <section class="sect_2">
+        <div class="sect_2__wrapper">
+            <div class="l-wrap">
+                <div class="sect_2_cont">
+                    <div class="sect_2_item">
+                        <p class="txt_concept">concept</p>
+                    </div>
+                    <div class="sect_2_item">
+                        <p class="txt_sub">
+                            おしゃれで<br>
+                            高性能な住宅を、<br>
+                            もっと手軽に。
+                        </p>
+                    </div>
+                    <div class="sect_2_item">
+                        <p class="txt_des">
+                            おしゃれで住みやすい家をライフスタイルに合わせて選ぶことができる。十分な性能と品質を備えた、お手頃価格の家。<br>
+                            <br>
+                            一般的に、高気密・高断熱、省エネ、高耐震といった<span>高性能住宅</span>は、そうでない住宅に比べてイニシャルコストが高くなりがちです。<br>
+                            より多くの方に、より<span>安心・安全で快適</span>な家に、<span>お手頃な価格</span>で住んでもらいたい。<br>
+                            <br>
+                            そこではるのいえでは、企画住宅の取り扱いをスタートいたしました。
+                        </p>
+
+                        <div class="sect_2_sub_cont">
+                            <div class="sect_2_sub_item">
+                                <div class="sub_itm">
+                                    <p>
+                                        POINT<br>
+                                        <span>01</span>
+                                    </p>
+                                </div>
+                                <div class="sub_itm">
+                                    <p>
+                                        <span>多数あるプラン</span>の中から<br>
+                                        ライフスタイルに合わせて選べる
+                                    </p>
+                                    <img src="<?php echo get_template_directory_uri();?>/release/image/sect_2/sect_2_itm_1.png" alt="">
+                                </div>
+                            </div>
+                            <div class="sect_2_sub_item">
+                                <div class="sub_itm">
+                                    <p>
+                                        POINT<br>
+                                        <span>02</span>
+                                    </p>
+                                </div>
+                                <div class="sub_itm">
+                                    <p>
+                                        同じデザイン、間取り、性能でも<br>
+                                        自由設計より<span>価格がお手頃</span>
+                                    </p>
+                                    <img src="<?php echo get_template_directory_uri();?>/release/image/sect_2/sect_2_itm_2.png" alt="">
+                                </div>
+                            </div>
+                            <div class="sect_2_sub_item">
+                                <div class="sub_itm">
+                                    <p>
+                                        POINT<br>
+                                        <span>03</span>
+                                    </p>
+                                </div>
+                                <div class="sub_itm">
+                                    <p>
+                                        全棟『<span>耐震等級3</span>』の高耐震、<br>
+                                        『<span>HEAT20 G2</span>』対応の<span>高断熱</span>
+                                    </p>
+                                    <img src="<?php echo get_template_directory_uri();?>/release/image/sect_2/sect_2_itm_3.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>       
+    </section>
     <section>3</section>
     <section>4</section>
     <section>5</section>
